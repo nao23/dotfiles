1fff495cc992d5083b803f718425f1cc93c6c780
Naofumi MURATA <10278628+nao23@users.noreply.github.com>
2021-12-25 16:33:16 +0900

    warning timeout setting for direnv

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -13,3 +13,5 @@ export PATH=/usr/local/opt/findutils/libexec/gnubin:${PATH}
 export MANPATH=/usr/local/opt/findutils/libexec/gnuman:${MANPATH}
 export PATH=/usr/local/opt/coreutils/libexec/gnubin:${PATH}
 export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}
+## direnv
+export DIRENV_WARN_TIMEOUT=100s
