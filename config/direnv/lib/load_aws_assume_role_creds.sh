0c578ea7766cd2ba3410f16094267b33c8bfa2e2
Naofumi MURATA <10278628+nao23@users.noreply.github.com>
2023-08-15 23:10:47 +0900

    export AWS_REGION

--- config/direnv/lib/load_aws_assume_role_creds.sh
+++ config/direnv/lib/load_aws_assume_role_creds.sh
@@ -6,6 +6,7 @@ function load_aws_assume_role_creds() {
 	AWS_ACCOUNT_ID=$(aws --profile $1 sts get-caller-identity | jq -r .Account)
 	CREDENTIAL=$(grep -h $AWS_ACCOUNT_ID ~/.aws/cli/cache/*)
 	export AWS_DEFAULT_REGION=ap-northeast-1
+	export AWS_REGION=ap-northeast-1
 	export AWS_PROFILE=$1
 	export AWS_ACCESS_KEY_ID=$(echo ${CREDENTIAL} | jq -r .Credentials.AccessKeyId)
 	export AWS_SECRET_ACCESS_KEY=$(echo ${CREDENTIAL} | jq -r .Credentials.SecretAccessKey)
