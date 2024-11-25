61d730f370c2a5dccfcea58fde3162c4ae5e5faf
Naofumi MURATA <10278628+nao23@users.noreply.github.com>
2024-11-25 12:04:56 +0900

    update alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,7 @@
 # Aliases
-alias jump='anyframe-widget-cdr'
 alias cat='bat'
 alias ls='eza'
 alias ll='eza -l'
 alias gs='anyframe-widget-checkout-git-branch'
+alias dc='docker compose'
+alias tf='terraform'
